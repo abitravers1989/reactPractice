549607a71fab7a8e9d3f25754e5034a7d29fe6c6
Abi Travers <atravers4007@gmail.com>
2018-09-06 21:44:23 +0100

    Starting react app

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+    render() {
+      return (
+        <div> 
+            <h1> Borneo Plans </h1>
+            <p> find out where we are going </p>
+        </div>
+      );
+    }
+  }
+
+  export default Titles;
